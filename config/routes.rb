4492bd3afd7b3edc4753f8ae00251dbc6e5b0a97
Franklin Kipchumba <sitieneifranklin@gmail.com>
2023-03-02 14:45:33 +0300

    Movie Now

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 namespace :api do
   namespace :controller do
-    resources :users, only: [:create]
-    resources :movies, only: [:index, :create, :update, :destroy]
+    resources :user, only: [:create]
+    resources :movie, only: [:index, :create, :update, :destroy]
     post '/login', to: 'auth#create'
-    get '/profile', to: 'users#profile'
-    get '/movies', to: 'movies#index'
-    get '/movies/search', to: 'movies#search'
+    get '/profile', to: 'user#profile'
+    get '/movie', to: 'movie#index'
+    get '/movie/search', to: 'movie#search'
   end
 end
