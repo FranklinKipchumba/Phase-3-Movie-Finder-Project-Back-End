726e36a75a17fb4c4c26abe0b82e7b68e889694c
Franklin Kipchumba <sitieneifranklin@gmail.com>
2023-03-02 16:33:50 +0300

    Movie Now

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@ movie1 = Movie.create(title: 'The Shawshank Redemption', year: 1994, description
 movie2 = Movie.create(title: 'The Godfather', year: 1972, description: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.', user: user1)
 movie3 = Movie.create(title: 'The Dark Knight', year: 2008, description: 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.', user: user2)
 
-Review.create(user_id: user1.id, movie_id: movie1.id, star_rating: 4, comment: "Nice product")
-Review.create(user_id: user2.id, movie_id: movie2.id, star_rating: 5, comment: "Great product")
-Review.create(user_id: user1.id, movie_id: movie3.id, star_rating: 3, comment: "OK product")
+Reviews.create(user_id: user1.id, movie_id: movie1.id, star_rating: 4, comment: "Nice product")
+Reviews.create(user_id: user2.id, movie_id: movie2.id, star_rating: 5, comment: "Great product")
+Reviews.create(user_id: user1.id, movie_id: movie3.id, star_rating: 3, comment: "OK product")
 
 puts "✅ Done seeding!"
