43e4b92c3a9db86968b1a8bcc63f9f5966d79027
Franklin Kipchumba <sitieneifranklin@gmail.com>
2023-03-02 13:50:49 +0300

    Movie Now

---
+++ app/controller/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < Sinatra::Base
+    set :default_content_type, 'application/json' 
+   
+    configure do
+      set :sessions, true
+      set :session_secret, ENV["SESSION_SECRET"]
+    end
+end
--- app/controller/auth_controller.rb
+++ app/controller/auth_controller.rb
@@ -1,18 +1,21 @@
 class AuthController < ApplicationController
-    def login
-      user = User.find_by(email: params[:email])
-      if user && user.authenticate(params[:password])
-        token = encode_token(user_id: user.id)
-        render json: { token: token }
-      else
-        render json: { error: 'Invalid email or password' }
-      end
+  post "/users/login" do
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      token = encode_token(user_id: user.id)
+      render json: { token: token }
+    else
+      render json: { error: "Invalid email or password" }
     end
-  
-    private
-  
-    def encode_token(payload)
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)
-    end
+  end
+  delete "/users/logout" do
+    session.clear
+    session.to_json
+  end
+
+  private
+
+  def encode_token(payload)
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+  end
 end
-  
