86ce2e8ab2382efa0a34179d2c66ed438dc99987
Franklin Kipchumba <sitieneifranklin@gmail.com>
2023-03-02 16:29:58 +0300

    Movie Now

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,6 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
-  validates :username, :presence => true, :uniqueness => true
+  validates :user_name, :presence => true, :uniqueness => true
   validates :password, :presence => true
 end
--- db/migrate/20230302102700_create_movies.rb
+++ db/migrate/20230302102700_create_movies.rb
@@ -1,4 +1,4 @@
-class CreateMovies < ActiveRecord::Migration[7.0]
+class CreateMovies < ActiveRecord::Migration[6.1]
   def change
     create_table :movie do |t|
       t.text :title
